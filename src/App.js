c00c586584a5439cc1e56781cb3304bbc94409ee
DESKTOP-E8RER0K\lior4 <lior456@gmail.com>
2018-08-06 18:52:12 +0300

    removing firebase

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import firebase from 'firebase';
 import ReduxThunk from 'redux-thunk';
 import reducers from './reducers';
 import logo from './logo.svg';
