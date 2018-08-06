3088733c2a35fff2a9bb0fb57df34ae2dadc33cd
DESKTOP-E8RER0K\lior4 <lior456@gmail.com>
2018-08-06 19:10:02 +0300

    test the book fetch action

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
+            <h1 className="App-title">Welcome to </h1>
           </header>
           <p className="App-intro">
             To get started, edit <code>src/App.js</code> and save to reload.
@@ -29,4 +29,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect(null, {bookFetch})(App);
