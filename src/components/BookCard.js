fcae5dfb9f14b22c6baf901b89f0fa40a6b5e44e
DESKTOP-E8RER0K\lior4 <lior456@gmail.com>
2018-08-10 16:27:17 +0300

    test

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -41,7 +41,7 @@ class BookCard extends Component {
   render() {
     const { title, authorName, publishedDate } = this.props.book;
     return (
-       <Col xm="12" xs="3">
+       <Col md="3" xs="12">
          <Card >
           <CardImg width="100%" src={background} />
           <CardImgOverlay>
